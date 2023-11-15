62388496b6b30e652bf7ecfba301fdb921b2cf8c
Aimé T Shangula <31923592+aimazin@users.noreply.github.com>
2023-11-15 22:22:56 +0100

    Add files via upload

---
+++ projectviii.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import random
+import openai 
+
+#
+
+st.title("💬 Chat with My AI Assistant")
+
+# Insert a chat message container.
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget.
+st.chat_input("I am Aime's resume know me")
+
+# Get the variables from constants.py
+pronoun = info['Pronoun']
+name = info['Name']
+subject = info['Subject']
+full_name = info['Full_Name']
+
+# Initialize the chat history
+if "messages" not in st.session_state:
+    welcome_msg = f"Hi! I'm {name}'s AI Assistant, Buddy. How may I assist you today?"
+    st.session_state.messages = [{"role": "assistant", "content": welcome_msg}]
+
+# App sidebar
+
+# First
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot") if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
+
+    st.caption(f"© Made by {full_name} 2023. All rights reserved.")
